77750a674ee1d569a3c8aff43c4cb9e0a94d3adc
Rahul Sharma <rahulsharma243998@gmail.com>
2024-11-30 13:35:21 +0530

    portfolio updated

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // *=============================== toggle icon navbar ==================================
+// Select the menu icon (hamburger) and nav menu
+const menuIcon = document.getElementById('menu-icon');
+const navMenu = document.getElementById('nav-menu');
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('#nav-menu');
-
-menuIcon.onclick = () => {
+// Add event listener to the menu icon
+menuIcon.addEventListener('click', function() {
+    // Toggle the 'nav-active' class to show or hide the menu
+    navMenu.classList.toggle('nav-active');
+    
+    // Toggle the 'bx-x' class to change the hamburger icon to 'X'
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
+});
 
 // *=============================== github calender ==================================
 GitHubCalendar(".calendar", "rahulsharma998");
@@ -15,7 +19,6 @@ GitHubCalendar(".calendar", "rahulsharma998");
 GitHubCalendar(".calendar", "rahulsharma998", { responsive: true });
 
 // *=============================== resume opening in a new tab ==================================
-
 let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab);
 let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab);
 
@@ -27,7 +30,6 @@ function NewTab() {
 }
 
 // *=============================== scroll sections active link ==================================
-
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
@@ -47,18 +49,15 @@ window.onscroll = () => {
     });
 
     // *=============================== sticky navbar ==================================
-
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    // *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
-
+    // *=============================== remove toggle icon and navbar when click navbar link (scroll) ==================================
     menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+    navMenu.classList.remove('nav-active');
 };
 
 // *=============================== scroll reveal ==================================
-
 ScrollReveal({
     distance: '80px',
     duration: 2000,
@@ -71,7 +70,6 @@ ScrollReveal().reveal('.home-content h1', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'left' });
 
 // *=============================== typed js ===================================
-
 const typed = new Typed('.multiple-text', {
     strings: ['Full Stack Web Developer', 'Frontend Developer'],
     typeSpeed: 100,
